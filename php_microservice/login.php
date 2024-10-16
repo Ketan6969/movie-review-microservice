f6c02494fefb97d83f2c4d4d9d5d58a5fd172945
Ketan6969 <solankiketan156@gmail.com>
2024-10-16 13:59:58 +0530

    test

---
+++ php_microservice/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+    <form action="login.php" method="post" class="login-form">
+        <input type="text" name="email" id="email" placeholder="Email">
+        <input type="text" name="password" id="password" placeholder="Password">
+        <input type="submit" value="Log in" name="submit">
+    </form>
+</body>
+</html>
+
