3a31a8ee11677c4a30ae99ea060b6c7ee19afdf0
Ketan6969 <solankiketan156@gmail.com>
2024-10-20 22:18:05 +0530

    Added the intitial Python service

--- flask_microservice/app.py
+++ flask_microservice/app.py
@@ -1,10 +1,29 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import mysql
+import mysql.connector
 
 app = Flask('__name__')
+def connect_db():
+    return mysql.connector.connect(host = 'localhost', user = 'root', password = 'tiger', database = 'test')
+
+@app.route("/getall", methods=['GET'])
+def addemp():
+    db = connect_db()
+    cur = db.cursor()
+    cur.execute('SELECT * FROM test')
+    result = cur.fetchall()
+    cur.close()
+    db.close()
+    return jsonify(result)
 
 @app.route('/')
 def home():
     return f'<h1>Welcome to homepage!!<h1>'
 
+# @app.route('/sign_up', methods = ['POST'])
+# def add_emp():
+#     db = connect_db()
+#     cur = db.cursor()
+#     cur.execute('insert into test')
 if __name__ == "__main__":
     app.run(debug=True)
--- php_microservice/login.php
+++ php_microservice/login.php
@@ -7,10 +7,19 @@
 </head>
 <body>
     <form action="login.php" method="post" class="login-form">
-        <input type="text" name="email" id="email" placeholder="Email">
-        <input type="text" name="password" id="password" placeholder="Password">
+        <input type="text" name="email" id="email" placeholder="Email" required>
+        <input type="text" name="password" id="password" placeholder="Password" required>
         <input type="submit" value="Log in" name="submit">
     </form>
 </body>
 </html>
+
+<?php
+    if($_SERVER['REQUEST_METHOD'] == "POST") {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+    }
+
+?>
 
